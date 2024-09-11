2f48cd4b8e61168410527a67da56f3a40d633b91
Alex Kucksdorf <akucksdorf@it-economics.de>
2024-09-11 09:19:48 +0200

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-training-2024-09-11--13" />
+    </div>
+  );
+}
+
+export default App;
