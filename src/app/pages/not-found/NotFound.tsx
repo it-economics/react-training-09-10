14a33ac0b27b6134e5106381f48e590d3694ecf8
Alex Kucksdorf <akucksdorf@it-economics.de>
2024-09-11 16:10:39 +0200

    Add not found page

---
+++ src/app/pages/not-found/NotFound.tsx
@@ -0,0 +1,6 @@
+import { Stack, Typography } from '@mui/material';
+
+export const NotFound = () => <Stack>
+  <Typography variant={"h1"}>404 - Not found!</Typography>
+  <Typography variant={"h2"}>This is not the page you are looking for!</Typography>
+</Stack>
--- src/app/router.tsx
+++ src/app/router.tsx
@@ -6,6 +6,7 @@ import { Home } from './pages/home/Home';
 
 const Joke = lazy(() => import('./pages/joke'));
 const StarWars = lazy(() => import('./pages/star-wars'));
+const NotFound = lazy(() => import('./pages/not-found'));
 
 export const router = createBrowserRouter([
   {
@@ -15,11 +16,12 @@ export const router = createBrowserRouter([
       { index: true, element: <Navigate to={'/home'} replace={true} /> },
       {
         path: 'home',
-        element: <Home />,
+        element: <Home />
       },
       { path: 'planets', element: <SolarSystem /> },
       { path: 'joke', element: <Joke /> },
       { path: 'star-wars', element: <StarWars /> },
-    ],
-  },
+      { path: '*', element: <NotFound />  }
+    ]
+  }
 ]);
