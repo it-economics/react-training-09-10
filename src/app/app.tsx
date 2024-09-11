75caefe249e53e6b54411244cda98d5e94bec8c8
Alex Kucksdorf <akucksdorf@it-economics.de>
2024-09-11 09:31:37 +0200

    remove nx component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="react-training-2024-09-11--13" />
+      <span>Hello!</span>
     </div>
   );
 }
