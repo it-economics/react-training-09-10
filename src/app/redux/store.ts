34d4ad9e730fdd545e5fd18f8b7ae276a8fe4d67
Vladimir Daskalov <vdaskalov@it-economics.de>
2024-09-12 15:35:48 +0200

    feat: add redux logger

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { issueTracketSlice } from '../issues/slice';
 import { homeSlice } from '../home/slice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
     home: homeSlice.reducer,
     issueTracker: issueTracketSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    process.env.NODE_ENV === 'production'
+      ? getDefaultMiddleware()
+      : getDefaultMiddleware().concat(logger),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
